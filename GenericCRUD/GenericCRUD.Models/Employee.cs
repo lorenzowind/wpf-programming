2bb793916078bf88427c1a95b942749bf30bd53b
lorenzowind <lorenzomart01@gmail.com>
2021-05-03 12:14:57 -0400

    feat (GenericCRUD): business logic layer

---
+++ GenericCRUD/GenericCRUD.Models/Employee.cs
@@ -0,0 +1,88 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GenericCRUD.Models
+{
+    [Serializable]
+    class Employee : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _email;
+        private string _phoneNumber;
+        private string _birthDate;
+        private string _role;
+        
+        public Employee()
+        {
+        }
+
+        public Employee(string name, string email, string phoneNumber, string birthDate, string role)
+        {
+            this._name = name;
+            this._email = email;
+            this._phoneNumber = phoneNumber;
+            this._birthDate = birthDate;
+            this._role = role;
+        }
+
+        public string Name 
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                _phoneNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string BirthDate
+        {
+            get { return _birthDate; }
+            set
+            {
+                _birthDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Role
+        {
+            get { return _role; }
+            set
+            {
+                _role = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ GenericCRUD/GenericCRUD.Models/Persistence.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GenericCRUD.Models
+{
+    class Persistence
+    {
+        public string FilePath { get; }
+        private List<Employee> _list = new List<Employee>();
+        
+        public Persistence(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public void Add(Employee employee)
+        {
+            _list.Add(employee);
+        }
+
+        public void Save(Employee employee, int index)
+        {
+            _list[index] = employee;
+        }
+
+        public void Remove(int index)
+        {
+            _list.RemoveAt(index);
+        }
+
+        public Employee this[int index] => _list[index];
+        public async Task Save()
+        {
+            var employeesJSON = JsonConvert.SerializeObject(_list);
+            JsonSerializer serializer = new JsonSerializer();
+            
+            using (var sr = new StreamWriter(employeesJSON))
+            using (var writer = new JsonTextWriter(sr))
+            {
+                await Task.Run(() => serializer.Serialize(writer, employeesJSON));
+            }
+        }
+
+        public async Task Load()
+        {
+            JsonSerializer serializer = new JsonSerializer();
+
+            using (var sr = new StreamReader(FilePath))
+            using (var reader = new JsonTextReader(sr))
+            {
+                var result = await Task.Run(() => serializer.Deserialize(reader));
+                _list = (List<Employee>) result;
+            }
+        }
+    }
+}
